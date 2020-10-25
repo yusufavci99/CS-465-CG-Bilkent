e96cf47dec7dcb4b0d6b1d820d8c86c3788a4eb0
yusufavci99 <yusufavci99@gmail.com>
2020-10-25 21:51:17 +0300

    Let's test.

--- Assignment 1/Code/kochPolygon.js
+++ Assignment 1/Code/kochPolygon.js
@@ -9,7 +9,7 @@ var fillPolygon = false;
 var renderPolygon = true;
 // Temporary Variable To Save Mouse Position Vector.
 var finished = false;
-
+// Temporarily stores mouse coordinates
 var mousePoint;
 // Stores the vertices of the polygon.
 var polygonVertices = [];
@@ -19,62 +19,71 @@ var curveVertices = [];
 var iterationCount = 2;
 // Background Color As RGB
 var backgroundColorRGB;
-// Color
+// RGB Color of the Polygon
 var polygonColorRGB;
+// RGB Color of the Koch curve
 var curveColorRGB;
-
+// Points to fragment shader color variable.
 var fColorPointer;
+// Points to vertex shader zoom variable.
 var zoomLocation;
+// How much zoom.
 var zoomMultiplier = 1;
+// Vertex data buffer.
 var bufferId;
+// Aspect Ratio of the program
 var aspectRatio;
 
+// Runs when the page loads.
 window.onload = function init() {
+
+    // Create a Canvas
     canvas = document.getElementById( "gl-canvas" );
     
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
     
-    
-    var a = document.getElementById("Button1")
+    // When the Polygon Is Completed, this button generates a Koch Curve
+    var a = document.getElementById("curveButton")
     a.addEventListener("click", function() {
 
         curveVertices = [];
 
+        // If polygon is not completed, show a warning.
         if (finished) {
             if (iterationCount > 0) {
+                // For All Edges, apply the Koch Rule (First Vertex is repeated because I am using LINE_STRIP)
                 for(let lineIndex = 0; lineIndex < polygonVertices.length - 1; lineIndex++) {
                     koch(polygonVertices[lineIndex], polygonVertices[lineIndex + 1], iterationCount);
                 }
             }
+            // If iteration Count is 0 don't apply the rule. (Apply 0 times.)
             else {
                 curveVertices = polygonVertices;
             }
         } else {
             alert("You Should Complete The Polygon");
         }
-
-    
         render();
     });
 
-
+    // True if currently dragging.
     let drawing = false;
-    finished = false;
-
     canvas.addEventListener("mousedown", function(event){
-        
 
+        // Restrart if there was a finished Polygon.
         if (finished) {
             polygonVertices = [];
             curveVertices = [];
             finished = false;
         }
 
+        // Add a new Vertex
         if (drawing) {
             drawing = false;
             polygonVertices.pop();
 
+            // If Close to the first vertex, complete polygon.
             if( length(subtract(mousePoint, polygonVertices[0])) < 0.05) {
                 polygonVertices.push(vec2(polygonVertices[0][0], polygonVertices[0][1]));
                 drawing = false;
@@ -88,6 +97,7 @@ window.onload = function init() {
         mousePoint = vec2(2*event.clientX/canvas.width-1, 
            2*(canvas.height-event.clientY)/canvas.height-1);
         
+        // First Push For adding a vertex, second for dragging.
         polygonVertices.push(mousePoint);
         polygonVertices.push(mousePoint);
 
@@ -95,12 +105,14 @@ window.onload = function init() {
         
     } );
 
+    // To follow the mouse.
     canvas.addEventListener("mousemove", function(event){
 
         if(drawing) {
             mousePoint = vec2(2*event.clientX/canvas.width-1, 
             2*(canvas.height-event.clientY)/canvas.height-1);
 
+            // Change the last vertex
             polygonVertices.pop();
             polygonVertices.push(mousePoint);
             
@@ -108,37 +120,39 @@ window.onload = function init() {
         }
     } );
 
+    // Set Aspect Ratio and Viewport length.
     gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-    //gl.viewport( 0, 0, canvas.width, canvas.height );
     aspectRatio = canvas.clientWidth / canvas.clientHeight;
 
 
-    //get selected colors in hex
+    // Get BG color in hex.
     let backgroundColorHex = document.getElementById("backgroundColor").value;
-    //convert hex colors into rgb
+    // Convert BG color to RGB.
     backgroundColorRGB = hexToRgb(backgroundColorHex);
 
+    // Get Polygon color in hex.
     let polygonColorHex = document.getElementById("polygonColor").value;
+    // Convert Polygon Color to RGB
     polygonColorRGB = hexToRgb(polygonColorHex);
 
+    // Get Curve color in hex.
     let curveColorHex = document.getElementById("curveColor").value;
+    // Convert Curve Color to RGB.
     curveColorRGB = hexToRgb(curveColorHex);
 
-    //clear buffer with new color
+    // Set Clear Color to BG Color
     gl.clearColor(backgroundColorRGB.r / 255.0, backgroundColorRGB.g / 255.0, backgroundColorRGB.b / 255.0, 1.0);
 
+    // Clear buffer with new color
     gl.clear( gl.COLOR_BUFFER_BIT );
 
-    //
     //  Load shaders and initialize attribute buffers
-    //
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     fColorPointer = gl.getUniformLocation(program, "fColor");
     zoomLocation = gl.getUniformLocation(program, "zoomC");
 
     gl.useProgram( program );
 
-  
     bufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
     var vPos = gl.getAttribLocation( program, "vPosition" );
@@ -154,6 +168,8 @@ window.onload = function init() {
             polygonColor: polygonColorRGB,
             curveColor: curveColorRGB,
             iteration: iterationCount,
+            fill: fillPolygon,
+            show: renderPolygon,
             vertices: polygonVertices
         }),
         "koch.txt", "text/plain");
@@ -169,18 +185,22 @@ window.onload = function init() {
             return function(e) {
                 var contents = e.target.result;
                 
-                console.log(JSON.parse(contents));
                 let loadedContent = JSON.parse(contents);
                 polygonVertices = loadedContent.vertices;
                 backgroundColorRGB = loadedContent.backgroundColor;
                 polygonColorRGB = loadedContent.polygonColor;
                 curveColorRGB = loadedContent.curveColor;
                 iterationCount = loadedContent.iteration;
-
+                fillPolygon = loadedContent.fill;
+                renderPolygon = loadedContent.show;
                 finished = equal(polygonVertices[0], polygonVertices[polygonVertices.length - 1]);
                 gl.clearColor(backgroundColorRGB.r / 255.0, backgroundColorRGB.g / 255.0, backgroundColorRGB.b / 255.0, 1.0);
 
-                document.getElementById("Button1").click();
+                document.getElementById("fillPolygon").checked = fillPolygon;
+                document.getElementById("showPolygon").checked = renderPolygon;
+
+                // Re-do the rules.
+                document.getElementById("curveButton").click();
             };
         })(this.files[0]);
         r.readAsText(this.files[0]);
@@ -223,7 +243,7 @@ window.onload = function init() {
 
     // ZOOMING
 
-    //StackOverFlow
+    // Took From StackOverFlow and modified (Keypress functionality)
     window.addEventListener("keydown", function (event) {
         if (event.defaultPrevented) {
             return; // Do nothing if the event was already processed
@@ -285,17 +305,20 @@ window.onload = function init() {
 
 }
 
+// Renders Everything.
 function render() {
     
+    // Clear to Background Color
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
 
+    // Set zoom shader variable.
     gl.uniform1f(zoomLocation, zoomMultiplier);
 
     // Loading Polygon Data To The Buffer
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(polygonVertices), gl.STATIC_DRAW );
 
-    // change fragment shader during render time
+    // Set Polygon Color Shader Variable
     gl.uniform4f(fColorPointer, polygonColorRGB.r / 255.0, polygonColorRGB.g / 255.0, polygonColorRGB.b / 255.0, 1.0);
 
     // Drawing The Polygon
@@ -314,13 +337,20 @@ function render() {
     // Drawing The Curve
     if(curveVertices.length != 0) {
 
-        // change fragment shader during render time
+        // Set Curve Color Shader Variable
         gl.uniform4f(fColorPointer, curveColorRGB.r / 255.0, curveColorRGB.g / 255.0, curveColorRGB.b / 255.0, 1.0);
         gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
         gl.bufferData( gl.ARRAY_BUFFER, flatten(curveVertices), gl.STATIC_DRAW );
         gl.drawArrays( gl.LINE_STRIP, 0, curveVertices.length );
     }
 }
+
+/**
+ * Generates vertexes to create a Koch Curve and pushes to curveVertices
+ * @param {*} point1 First point on the line.
+ * @param {*} point9 Final point on the line.
+ * @param {*} iteration Number of iterations.
+ */
 
 function koch(point1, point9, iteration) {
 
@@ -329,8 +359,9 @@ function koch(point1, point9, iteration) {
     // Calculate Point 2
     let point2 = mix(point1, point9, 0.25);
 
-    // Calculate Point 3
+    // Calculate Point 3 (Rotating Point 1 Around Point 2 by 90 Degrees)
     let temp = subtract(point1, point2);
+    // For Aspect Ratio
     temp = vec2(temp[1] / aspectRatio, -temp[0] * aspectRatio);
     let point3 = add( temp, point2);
 
@@ -382,9 +413,13 @@ function koch(point1, point9, iteration) {
     }
 }
 
-/*
- * method that translates hex codes into RGB and returns them as an array. Taken from:
+// Functions Taken From StackOverFlow
+
+/**
+ * Translates Hex Color Code To RGB Color Code. Source:
  * https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+ * @param {*} hex Hex Code
+ * @returns RGB Color Code
  */
 function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
@@ -395,6 +430,13 @@ function hexToRgb(hex) {
     } : alert('color value not correct');
 }
 
+/**
+ * Downloads a Content. Source:
+ * https://stackoverflow.com/questions/4184944/javascript-download-data-to-file-from-content-within-the-page
+ * @param {*} content Content To Be Downloaded as String
+ * @param {*} fileName Name of Downloaded File
+ * @param {*} contentType Type of the Downloaded File
+ */
 function download(content, fileName, contentType) {
     var a = document.createElement("a");
     var file = new Blob([content], {type: contentType});
